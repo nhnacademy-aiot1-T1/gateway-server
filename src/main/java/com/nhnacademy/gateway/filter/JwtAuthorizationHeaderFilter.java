9d7b59e230ed1d327ee3e931f61a9d46c3b78819
tothemoon9954 <tothemoon9954>
2024-03-29 17:11:29 +0900

    feat: Access Token 만료확인코드 추가

--- src/main/java/com/nhnacademy/gateway/filter/JwtAuthorizationHeaderFilter.java
+++ src/main/java/com/nhnacademy/gateway/filter/JwtAuthorizationHeaderFilter.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.gateway.filter;
 
+import com.nhnacademy.gateway.key.JwtProperty;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
@@ -12,20 +13,23 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 import io.jsonwebtoken.security.Keys;
 
+import java.util.Date;
+
 @Slf4j
 @Component
 public class JwtAuthorizationHeaderFilter extends AbstractGatewayFilterFactory<JwtAuthorizationHeaderFilter.Config> {
 
-    private String jwtSecretKey = "secret-Key";
+    private JwtProperty jwtProperty;
+
     public JwtAuthorizationHeaderFilter(){
         super(Config.class);
     }
 
     public static class Config{
-        private int inat;
+        private String key;
 
-        public int getInat(){
-            return this.inat;
+        public String getKey(){
+            return this.key;
         }
     }
 
@@ -45,20 +49,22 @@ public class JwtAuthorizationHeaderFilter extends AbstractGatewayFilterFactory<J
 
             try {
                 Claims claims = Jwts.parserBuilder()
-                        .setSigningKey(Keys.hmacShaKeyFor(jwtSecretKey.getBytes()))
+                        .setSigningKey(Keys.hmacShaKeyFor(jwtProperty.getSecretKey().getBytes()))
                         .build()
                         .parseClaimsJws(jwtToken)
                         .getBody();
 
+                Date expirationDate = claims.getExpiration();
+                if (expirationDate !=null && expirationDate.before(new Date())){
+                    throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Access 토큰만료");
+                }
 
 
                 exchange = exchange.mutate().request(builder -> builder.header("X-USER-ID", claims.getSubject())).build();
 
-
             } catch (Exception e) {
                 throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
             }
-
             return chain.filter(exchange);
         };
     }
